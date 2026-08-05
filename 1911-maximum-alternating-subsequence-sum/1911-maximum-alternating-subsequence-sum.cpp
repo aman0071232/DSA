ba63e90e5696aa3b66b27a63c07843a76c0fbfaa
aman0071232 <amanguptanew06112005@gmail.com>
2026-08-06 01:01:59 +0530

    Time: 94 ms (99.74%), Space: 94.9 MB (91.5%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        long long even=0,odd=0;
+
+        for(int num:nums){
+            long long newEven=max(even,odd+num);
+            long long newOdd=max(odd,even-num);
+
+            even=newEven;
+            odd=newOdd;
+        }
+        return even;
+    }
+};
