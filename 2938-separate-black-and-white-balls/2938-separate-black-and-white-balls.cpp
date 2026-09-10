eca392af76438025f7be981a004e4e9f35af59a1
pseudo <amanguptanew06112005@gmail.com>
2026-09-10 19:06:26 +0530

    Time: 3 ms (85.2%), Space: 17.3 MB (41.61%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long j=0;
+        int n=s.size();
+       long long count=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0' ){
+                swap(s[i],s[j]);
+                count+=i-j;
+                j++;
+            }
+        }
+        return count;
+    }
+};
