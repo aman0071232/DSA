99e9a8364ba3163a10eb8e4a9395ebf8783c7714
pseudo <amanguptanew06112005@gmail.com>
2026-09-10 19:41:51 +0530

    Time: 0 ms (100%), Space: 15.8 MB (54.36%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int piv = -1;
+        int n=nums.size();
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i]) {
+                piv = i;
+                break;
+            }
+        }
+        if (piv ==-1) {
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        for(int i=n-1;i>piv;i--){
+            if(nums[i]>nums[piv]){
+                swap(nums[i],nums[piv]);
+                break;
+            }
+        }
+        int i=piv+1;
+        int j=nums.size()-1;
+        while(i<=j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+        return ;
+    }
+};
