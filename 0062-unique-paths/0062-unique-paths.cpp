2ca24d1670032958c2564dfcab02f2db48242b5e
pseudo <amanguptanew06112005@gmail.com>
2026-09-12 18:11:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.15%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,13 +4,13 @@ public:
     int solve(int i, int j, int m, int n) {
 
         if (i == m - 1 && j == n - 1) {
-            return t[i][j] = 1;
+            return 1;
         }
         if (t[i][j] != -1) {
             return t[i][j];
         }
         if (i < 0 || j < 0 || i >= m || j >= n) {
-            return t[i][j] = 0;
+            return 0;
         }
         int right = solve(i, j + 1, m, n);
         int down = solve(i + 1, j, m, n);
