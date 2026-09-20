36c4e012e02172e3d3f002c9ec049ad61dd2522b
pseudo <amanguptanew06112005@gmail.com>
2026-09-21 00:09:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.45%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((start & (1 << i) )!= (goal & (1 << i))) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+};
