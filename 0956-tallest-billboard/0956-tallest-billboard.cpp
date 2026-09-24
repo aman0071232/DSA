469853544e27cb5be342fecf12e479bbe473dc44
pseudo <amanguptanew06112005@gmail.com>
2026-09-25 00:47:30 +0530

    Time: 69 ms (56.84%), Space: 12.3 MB (71.91%) - LeetHub

---
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   int n;
+   int t[21][10003];
+   int solve(vector<int>&rods,int idx,int diff){
+    if(idx>=n){
+        if(diff==0){
+            return 0;
+        }
+        return INT_MIN;
+    }
+    if(t[idx][diff+5000]!=-1){
+        return t[idx][diff+5000];
+    }
+    int nothing=solve(rods,idx+1,diff);
+    int in_left_rod=rods[idx]+solve(rods,idx+1,diff+rods[idx]);
+
+    int in_right_rod=rods[idx]+solve(rods,idx+1,diff-rods[idx]);
+    return t[idx][diff+5000]=max({nothing,in_left_rod,in_right_rod});
+   }
+    int tallestBillboard(vector<int>& rods) {
+        n=rods.size();
+        memset(t,-1,sizeof(t));
+        return solve(rods,0,0)/2;
+    }
+};
