3dc704c09c36866483cc612ed0af0e7d41f75c8a
pseudo <amanguptanew06112005@gmail.com>
2026-09-25 19:53:10 +0530

    Time: 31 ms (98.59%), Space: 18.1 MB (99.01%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-
-        int result = 0;
-
-        unordered_map<long, int> mp[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                long diff = (long)nums[i] - nums[j];
-
-                auto it = mp[j].find(diff);
-                int count_at_j = it == end(mp[j]) ? 0 : it->second;
-                mp[i][diff] += count_at_j + 1;
-                result += count_at_j;
+        static constexpr size_t N{1000};
+        
+        static array<uint, N> next;
+        static array<array<int, N>, N> dp;
+        
+        const auto n{nums.size()};
+        if (n < 3)
+            return 0;
+        
+        unordered_map<int, uint> first_id{};
+        first_id.reserve(n << 1);
+        for (size_t i{n - 1}, sz{sizeof(int) * n}; ~i; --i) {
+            memset(dp[i].data(), 0, sz);
+            auto& j{first_id[nums[i]]};
+            if (j == 0)
+                j = ~0;
+                
+            next[i] = exchange(j, i);
+        }
+  
+        int ans{};
+        for (size_t i{2}; i != n; ++i) {
+            const auto v2{nums[i]};
+            auto* dp2{dp[i].data()};
+            for (size_t j{1}; j != i; ++j) {
+                const auto v0{(static_cast<int64_t>(nums[j]) << 1) - v2};
+                if (v0 > INT_MAX || v0 < INT_MIN)
+                    continue;
+                    
+                const auto it{first_id.find(v0)};
+                if (it == first_id.end())
+                    continue;
+                
+                int s{};
+                const auto* dp0{dp[j].data()};
+                for (auto k{it->second}; k < j; k = next[k]) {
+                    s += dp0[k] + 1;
+                }
+                ans += dp2[j] = s;
             }
         }
-        return result;
+
+        return ans;
     }
 };
